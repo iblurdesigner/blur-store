56fa4ade1b0084bf96aa31d1cec8ef29b138238e
iblurdesigner <nevomen@gmail.com>
2021-03-02 18:50:51 -0500

    Agregado el Link de React Router y los iconos con Fontawesome

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import '../styles/components/Header.css'
 
 const Header = () => (
   <div className="Header">
-    <h1 className="Header-title">Blur Store</h1>
-    <div className="Header-checkout">Checkout</div>
+    <Link to="/">
+      <h1 className="Header-title">Blur Merch Store</h1>
+    </Link>
+    <div className="Header-checkout">
+      <Link to="/checkout">
+        <i className="fas fa-shopping-basket" />
+      </Link>
+    </div>
   </div>
 )
 
--- src/containers/Information.jsx
+++ src/containers/Information.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import '../styles/components/Information.css'
 
 const Information = () => (
@@ -22,7 +23,9 @@ const Information = () => (
       </div>
       <div className="Information-buttons">
         <div className="Information-back">Regresar</div>
-        <div className="Information-next">Pagar</div>
+        <Link to="/checkout/payment">
+          <div className="Information-next">Pagar</div>
+        </Link>
       </div>
     </div>
     <div className="Information-sidebar">
