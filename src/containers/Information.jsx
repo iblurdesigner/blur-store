00169a5501df80748aa10afae0f3b1138f083c8f
iblurdesigner <nevomen@gmail.com>
2021-03-02 18:19:17 -0500

    Informacion del usuario

--- src/containers/Information.jsx
+++ src/containers/Information.jsx
@@ -1,5 +1,38 @@
 import React from 'react'
+import '../styles/components/Information.css'
 
-const Information = () => <h1>Information</h1>
+const Information = () => (
+  <div className="Information">
+    <div className="Information-content">
+      <div className="Information-head">
+        <h2>Informacion de contacto</h2>
+      </div>
+      <div className="Information-form">
+        <form action="">
+          <input type="text" placeholder="Nombre Completo" name="name" />
+          <input type="text" placeholder="Correo electronico" name="email" />
+          <input type="text" placeholder="Direccion" name="address" />
+          <input type="text" placeholder="apto" name="apto" />
+          <input type="text" placeholder="Ciudad" name="city" />
+          <input type="text" placeholder="Pais" name="country" />
+          <input type="text" placeholder="Provincia" name="state" />
+          <input type="text" placeholder="Codigo Postal" name="cp" />
+          <input type="text" placeholder="Telefono" name="phone" />
+        </form>
+      </div>
+      <div className="Information-buttons">
+        <div className="Information-back">Regresar</div>
+        <div className="Information-next">Pagar</div>
+      </div>
+    </div>
+    <div className="Information-sidebar">
+      <h3>Pedido:</h3>
+      <div className="Information-element">
+        <h4>ITEM Name</h4>
+        <span>$10</span>
+      </div>
+    </div>
+  </div>
+)
 
 export default Information
