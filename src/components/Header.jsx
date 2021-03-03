235a22e306817798f00286419d018df6c9210dc1
iblurdesigner <nevomen@gmail.com>
2021-03-03 16:40:53 -0500

    Implementando useContext

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,26 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Link } from 'react-router-dom'
+import AppContext from '../context/AppContext'
 import '../styles/components/Header.css'
 
-const Header = () => (
-  <div className="Header">
-    <Link to="/">
-      <h1 className="Header-title">Blur Merch Store</h1>
-    </Link>
-    <div className="Header-checkout">
-      <Link to="/checkout">
-        <i className="fas fa-shopping-basket" />
-      </Link>
+const Header = () => {
+  const { state } = useContext(AppContext)
+
+  const { cart } = state
+
+  return (
+    <div className="Header">
+      <h1 className="Header-title">
+        <Link to="/">Blur Merch Store</Link>
+      </h1>
+      <div className="Header-checkout">
+        <Link to="/checkout">
+          <i className="fas fa-shopping-basket" />
+        </Link>
+        {cart.length > 0 && <div className="Header-alert">{cart.length}</div>}
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default Header
